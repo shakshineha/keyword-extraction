ffc1b73cdb988b9b0d5987d9fa7dd5037364bd23
SHAKSHI NEHA <40252358+shakshineha@users.noreply.github.com>
2018-07-19 19:24:51 +0530

    Update keywordscoring.py

--- keywordscoring.py
+++ keywordscoring.py
@@ -2,30 +2,24 @@ from rake_nltk import Rake
 
 r = Rake()
 
-# importing required modules
+# importing required modules, here PyPDF2 as we want to read a PDF file
 import PyPDF2
-
+ 
 # creating a pdf file object
-pdfFileObj = open('C:/Users/HP/Downloads/JavaBasics-notes.pdf', 'rb') #This is the location of file I have been reading
+pdfFileObj = open('C:/Users/HP/Downloads/JavaBasics-notes.pdf', 'rb')
  
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
  
-# printing number of pages in pdf file
+# storing the number of pages available in that file in a variable called numb
 numb=pdfReader.numPages
-print(numb)
 
-# creating a page object
-#for i in range (numb):
-    #pageObj = pdfReader.getPage(i)
-    #extracting text from page
-    #print(pageObj.extractText())
-    
-for k in range (numb):
-    pageObj = pdfReader.getPage(k)    
-    r.extract_keywords_from_text(pageObj.extractText())
-    r.get_ranked_phrases()
-    # To get keyword phrases ranked highest to lowest with scores.
-    r.get_ranked_phrases_with_scores()
-    
-pdfFileObj.close()        
+
+for i in range (numb):
+    pageObj = pdfReader.getPage(i)# creating a page object, which will create a list of page objects for each page
+    print(pageObj.extractText())# extracting text from page, from each page one after the another
+    r.extract_keywords_from_text(pageObj.extractText())# Extracting keywords from the documents
+    phrases=r.get_ranked_phrases()#It will give all the keyworded phrases from the document
+    print(phrases)
+    phrases_scores=r.get_ranked_phrases_with_scores()#It will give the phrases with scores
+    print(phrases_scores)
